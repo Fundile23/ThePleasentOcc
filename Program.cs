dc2ecb9bca1214d82c211503b208451dbd509d3d
Fundile Bungane <fundilebungane0@gmail.com>
2026-07-19 15:50:24 +0200

    Switched from SQL Server to PostgreSQL

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSession(options =>
 
 // Add database context
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
